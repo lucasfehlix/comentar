bf2a2d170d845eceefde06a62c81d38d2c9e8784
lucasfehlix <lucasfehlix@gmail.com>
2019-10-25 23:41:50 -0300

    Atual

---
+++ discussao.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Discussão</title>
+        <link rel="stylesheet" href="css/discussao.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="">Entrar</a></li>
+            </ul>
+        </nav>
+        <div id="largura-pagina">
+            <section id="conteudo1">
+                <h1>Guia Definitivo Como Criar um Blog Incrivel e Ganhar Dinheiro Com Ele</h1>
+                <img src="img/computador.jpg">
+                <p class="texto">É um fato há muito estabelecido que um leitor se distrairá com o conteúdo legível de uma 
+                    página ao analisar seu layout. O ponto de usar o Lorem Ipsum é que ele tem uma distribuição 
+                    de letras mais ou menos normal, em vez de usar 'Conteúdo aqui, conteúdo aqui'.</p>
+                <p class="texto">1. O ponto de usar o Lorem Ipsum</p>
+                <p class="texto">2. È que ele tem uma distribuição de letras</p>
+                <p class="texto">3. Lorem Ipsum é que ele tem uma distribuição</p>
+                <p class="texto">4. letras mais ou menos normal</p>
+            </section>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Comentarios</title>
+        <link rel="stylesheet" href="css/estilo.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="discussao.php">Discução</a></li>
+                <li><a href="">Entrar</a></li>
+            </ul>
+        </nav>
+        <h3>Conteudo qualquer</h3>
+    </body>
+</html>
